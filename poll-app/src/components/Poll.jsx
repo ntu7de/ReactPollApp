5b5cabd0b9b9ce241cd0c09377d6a02ba5daf450
Patrick Yuson <pyuson@patricks-mbp.myfiosgateway.com>
2024-05-21 12:01:02 -0400

    upvote functionality appears to be working

--- poll-app/src/components/Poll.jsx
+++ poll-app/src/components/Poll.jsx
@@ -9,28 +9,36 @@ const Poll = () => {
     const [responses, setResponses] = useState([]); 
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
-        const docRef = await addDoc(collection(db, "answers"), {
-            answer: answer,
-            upvotes: 0,
-        });
-        setAnswer(""); 
+        try {
+            e.preventDefault();
+            const docRef = await addDoc(collection(db, "answers"), {
+                answer: answer,
+                upvotes: 0,
+            });
+            setAnswer(""); 
+            fetchResponses();
+        }
+        catch (error) {
+            console.error("Error adding document: ", error);
+        }
+       
+    };
+
+    const fetchResponses = async () => {
+        try {
+            const querySnapshot = await getDocs(collection(db, 'answers'));
+            const fetchedResponses = querySnapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+            setResponses(fetchedResponses);
+            console.log(fetchedResponses);
+        } catch (error) {
+            console.error('Error fetching responses: ', error);
+        }
     };
 
     useEffect(() => {
-        const fetchResponses = async () => {
-            try {
-                const querySnapshot = await getDocs(collection(db, 'answers'));
-                const fetchedResponses = querySnapshot.docs.map(doc => ({
-                    id: doc.id,
-                    ...doc.data()
-                }));
-                setResponses(fetchedResponses);
-                console.log(fetchedResponses);
-            } catch (error) {
-                console.error('Error fetching responses: ', error);
-            }
-        };
         fetchResponses();
     }, []);
 
@@ -59,65 +67,3 @@ const Poll = () => {
 };
 
 export default Poll;
-
-// import React from 'react'
-// import '../styles/Poll.css'
-// import { useState, useEffect } from "react";
-// import { db } from "../../firebase";
-// import { addDoc, collection, getDocs } from "firebase/firestore";
-// import Response from './Response'
-
-// const Poll = () => {
-//     const [answer, setAnswer] = useState("");
-//     const [responses, setResponses] = useState([]); 
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         const docRef = await addDoc(collection(db, "answers"), {
-//             answer: answer,
-//             upvotes: 0,
-//         });
-//         console.log("created doc with id: ", docRef.id);
-//     }
-//     useEffect(() => {
-//         const fetchResponses = async () => {
-//             try {
-//                 const data = await getDocs(collection(db, 'answers'));
-//                 setResponses(data._snapshot.docChanges);
-//                 // console.log(data._snapshot.docChanges[0].doc.data.value.mapValue.fields);
-//                 console.log(data._snapshot.docChanges)
-//             }
-//             catch (error) {
-//                 console.error('Error fetching responses: ', error);
-//             }
-//         }
-//         fetchResponses();
-//     }, [])
-        
-
-//     return (
-//         <>
-//             <h1>Poll City</h1>
-//             <h2>Where would you like to go on vacation this summer?</h2>
-//             <input 
-//                 placeholder="Type your answer!"
-//                 type='text'
-//                 onChange={(e) => setAnswer(e.target.value)}>
-//             </input>
-//             <br></br>
-//             <button type='submit'
-//                 onClick={handleSubmit}>Submit
-//             </button>
-//             <h3>Previous responses:</h3>
-//             {responses && responses.map(( response ) => {
-//                 return (
-//                     <div key={response.doc.version.timestamp.nanoseconds}>
-//                         <Response response={response}/>
-//                     </div>
-//                 )
-//             })}
-//         </>
-//     )
-// }
-
-// export default Poll
--- poll-app/src/components/Response.jsx
+++ poll-app/src/components/Response.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, IconButton, Typography } from '@mui/material';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import { db } from "../../firebase";
+import { updateDoc, doc } from "firebase/firestore";
 
 const Response = ({ response }) => {
+    const [upvotes, setUpvotes] = useState(response.upvotes);
+
+    const handleClick = async () => {
+        try{
+            const docRef = doc(db, 'answers', response.id);
+            const newUpvotes = response.upvotes + 1;
+            await updateDoc(docRef, {
+                upvotes: newUpvotes
+            });
+            setUpvotes(newUpvotes);
+        }
+        catch (error) {
+            console.error("Error updating document: ", error);
+        }
+        
+    };
 
     return (
         <>
-            <p>{response.answer} {response.upvotes}</p>
+            <Box sx={{ 
+                display: 'flex', 
+                mb: 2, 
+                gap: 4,
+                maxWidth: '600px', 
+                margin: 'auto', 
+                alignItems: 'center',
+            }}>
+                <Box sx={{ justifyContent: 'left', alignItems: 'left'}}>
+                    <Typography variant="body1">{response.answer}</Typography>
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'right', gap: 0.5 }}>
+                    <Typography variant="body1">{upvotes}</Typography>
+                    <IconButton onClick={handleClick}>
+                        <ThumbUpIcon />
+                    </IconButton>
+                </Box>
+            </Box>
         </>
     )
 }
